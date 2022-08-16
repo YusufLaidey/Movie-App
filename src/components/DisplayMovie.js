2cf56509ec7ef6a90f2fe7a9ef84d7809be2cc05
YusufLaidey <yusuflaideylee@gmail.com>
2022-08-16 11:15:05 +0100

    few changes

--- src/components/DisplayMovie.js
+++ src/components/DisplayMovie.js
@@ -7,7 +7,7 @@ const DisplayMovie = ({ value }) => {
       <Image url={value} />
       <div className="movie-details">
         <h3>Title: {value.original_title}</h3>
-        <p>overview: {value.overview}</p>
+        <p>Overview: {value.overview}</p>
         <p>Language: {value.original_language}</p>
         <p>Release_Date: {value.release_date}</p>
         <p>Ratings: {value.vote_average}</p>
