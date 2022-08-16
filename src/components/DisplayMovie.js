2a37bf32fc01e5cbe217eaa3ad318501bffeaa6d
YusufLaidey <yusuflaideylee@gmail.com>
2022-08-16 11:14:28 +0100

    added a few features

--- src/components/DisplayMovie.js
+++ src/components/DisplayMovie.js
@@ -6,11 +6,11 @@ const DisplayMovie = ({ value }) => {
     <div className="d">
       <Image url={value} />
       <div className="movie-details">
-        <h3>{value.original_title}</h3>
-        <p>{value.overview}</p>
+        <h3>Title: {value.original_title}</h3>
+        <p>overview: {value.overview}</p>
         <p>Language: {value.original_language}</p>
         <p>Release_Date: {value.release_date}</p>
-        <p>IMDB: {value.vote_average}</p>
+        <p>Ratings: {value.vote_average}</p>
       </div>
     </div>
   );
